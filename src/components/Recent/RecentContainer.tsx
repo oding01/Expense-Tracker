98b217fb50f58fed54e26e618ddafaae688dd537
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-03 11:12:12 +0900

    feat: px 기준 줄 넘김 -> breakpoint 기준 flex-row & col 변경

--- src/components/Recent/RecentContainer.tsx
+++ src/components/Recent/RecentContainer.tsx
@@ -3,11 +3,11 @@ import MostRecent from '@/components/Recent/MostRecent'
 
 export const RecentContainer = (): JSX.Element => {
 	return (
-		<div className='flex flex-wrap items-end gap-16 relative w-full'>
-			<div className='relative flex-1 min-w-[350px]'>
+		<div className='flex flex-col tablet:flex-row items-end gap-16 relative w-full'>
+			<div className='relative flex-1 min-w-auto w-full tablet:min-w-[300px]'>
 				<MostRecent consumePlace='롯데백화점' />
 			</div>
-			<div className='relative flex-1 min-w-[350px]'>
+			<div className='relative flex-1 min-w-auto w-full tablet:min-w-[300px]'>
 				<ConsumeList />
 			</div>
 		</div>
