b77cc4c13401cb49d0868b3315a90cbe90476114
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-03 11:13:08 +0900

    feat: width 최소치 수정 -> css 안깨짐

--- src/components/Expense/ExpenseContainer.tsx
+++ src/components/Expense/ExpenseContainer.tsx
@@ -4,10 +4,10 @@ import { Expense } from '@/components/Expense/Expense'
 export const ExpenseContainer = (): JSX.Element => {
 	return (
 		<div className='flex flex-col tablet:flex-row items-start gap-16 relative w-full'>
-			<div className='relative flex-1 min-w-[350px] w-full'>
+			<div className='relative flex-1 min-w-auto tablet:min-w-[300px] w-full '>
 				<Expense />
 			</div>
-			<div className='relative flex-1 min-w-[350px] w-full'>
+			<div className='relative flex-1 min-w-auto tablet:min-w-[300px] w-full'>
 				<CategoryChart />
 			</div>
 		</div>
