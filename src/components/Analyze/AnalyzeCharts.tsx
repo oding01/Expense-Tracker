fc8e0e79cbb3c940d9efed1231e71b1b3dc6696f
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-04 17:54:03 +0900

    feat: :sparkles: Add Basic printing function
    간단하게 변수 출력하는거 만들었음, 아직 기능 없삼

--- src/components/Analyze/AnalyzeCharts.tsx
+++ src/components/Analyze/AnalyzeCharts.tsx
@@ -1,3 +1,24 @@
-export const AnalyzeCharts = () => {
-	return <div>AnalyzCharts</div>
+interface analyzeDataType {
+	category: string
+	detail: string
+	price: number
+	date: string
+}
+
+export const AnalyzeCharts = ({
+	consumptionData,
+}: {
+	consumptionData: analyzeDataType[]
+}) => {
+	return (
+		<div className='flex-initial'>
+			<div>
+				{consumptionData.map((data) => (
+					<>
+						<div className='flex-initial'>{data.category}</div>
+					</>
+				))}
+			</div>
+		</div>
+	)
 }
--- src/components/Analyze/AnalyzeContainer.tsx
+++ src/components/Analyze/AnalyzeContainer.tsx
@@ -1,11 +1,12 @@
 import { AnalyzeCharts } from '@/components/Analyze/AnalyzeCharts'
 import { AnalyzeList } from '@/components/Analyze/AnalyzeList'
+import { consumptionData } from '@/Mock/Mock'
 
 export const AnalyzeContainer = () => {
 	return (
-		<>
-			<AnalyzeCharts />
+		<div className='flex flex-col w-full h-full justify-center items-center flex-initial bg-white rounded-[3rem] shadow-[24px_24px_48px_#74a2cc99,inset_-24px_-24px_48px_#c5deff,inset_12px_12px_24px_#e3efff]'>
+			<AnalyzeCharts consumptionData={consumptionData} />
 			<AnalyzeList />
-		</>
+		</div>
 	)
 }
