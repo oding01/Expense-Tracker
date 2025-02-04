54caad3440b52b4d4a9fcaed43c64337db6036e5
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-04 11:30:24 +0900

    Feat: Making Base Analyze Components

--- src/components/Analyze/AnalyzeContainer.tsx
+++ src/components/Analyze/AnalyzeContainer.tsx
@@ -1,7 +1,11 @@
+import { AnalyzCharts } from '@/components/Analyze/AnalyzeCharts'
+import { AnalyzeList } from '@/components/Analyze/AnalyzeList'
+
 export const AnalyzeContainer = () => {
 	return (
 		<>
-			<div>AnalyzeContainer</div>
+			<AnalyzCharts />
+			<AnalyzeList />
 		</>
 	)
 }
