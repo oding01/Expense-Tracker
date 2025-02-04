8fef968669ef57da46b79d8bdb3c545221256354
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-04 16:46:06 +0900

    test: :card_file_box: Add Analyze Data
    category, detail, price, date로 구성된 mock data 생성

---
+++ src/Mock/Mock.ts
@@ -0,0 +1,18 @@
+export const analyzeMockData = [
+	{ category: '편의점', detail: '물', price: 1000, date: '2025-01-02' },
+	{
+		category: '병원',
+		detail: '정형외과 진료',
+		price: 300000,
+		date: '2025-01-06',
+	},
+	{ category: '식사', detail: 'OO치킨', price: 250000, date: '2025-01-10' },
+	{ category: '유흥', detail: 'OO적셔', price: 33000, date: '2025-01-16' },
+	{ category: '쇼핑', detail: 'E북리더기', price: 120000, date: '2025-01-25' },
+	{
+		category: '쇼핑',
+		detail: 'E북리더기 악세서리',
+		price: 10000,
+		date: '2025-01-31',
+	},
+]
