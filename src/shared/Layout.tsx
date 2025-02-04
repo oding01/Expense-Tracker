403ff9eb0a66399bfc8fd739308e527ec72a7c80
이어진 <slimin92@naver.com>
2025-02-04 17:25:49 +0900

    feat: :sparkles: Header 추가 및 Layout 컴포넌트 추가

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -3,9 +3,12 @@ import { Outlet } from 'react-router'
 
 const Layout = () => {
 	return (
-		<div className='flex flex-col w-full items-center relative'>
+    <div className='flex flex-col min-h-screen min-w-full items-center relative'>
 			<Header />
-			<Outlet />
+      {/* flex-1로 Header를 제외한 남은 영역 채우기 */}
+			<div className='w-full pt-16 flex-1'>
+				<Outlet />
+			</div>
 		</div>
 	)
 }
