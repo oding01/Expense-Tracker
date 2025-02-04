91fd07deebb882beb84c50869798dfbe241a38aa
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-04 11:20:39 +0900

    style: Remove '/' in path

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import '@/index.css'
 import App from '@/App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import { Analyze } from '@/pages/Analyze'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<App />} />
+				<Route path='' element={<App />} />
+				<Route path='analyze' element={<Analyze />} />
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>,
