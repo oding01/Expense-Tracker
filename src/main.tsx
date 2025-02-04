9bf3f1a50c55d28751c280381b811c0b2ae313fd
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-04 11:51:46 +0900

    refactor: :bug: Remove App.tsx, App.css
    필요없는 App 구조 삭제하고 App.css 내용 index.css에 옮김

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/index.css'
-import App from '@/App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import { Analyze } from '@/pages/Analyze'
+import Home from '@/pages/Home'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Routes>
-				<Route path='' element={<App />} />
+				<Route index element={<Home />} />
 				<Route path='analyze' element={<Analyze />} />
 			</Routes>
 		</BrowserRouter>
