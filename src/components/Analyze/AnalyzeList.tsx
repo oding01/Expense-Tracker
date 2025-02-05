a9e73d940c39422f33b374c65cddcd7dcf3f1335
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-05 21:50:08 +0900

    feat: :art: 레이아웃 완성, 디자인 미완성

--- src/components/Analyze/AnalyzeList.tsx
+++ src/components/Analyze/AnalyzeList.tsx
@@ -1,3 +1,23 @@
-export const AnalyzeList = () => {
-	return <div>AnalyzeList</div>
+interface analyzeDataType {
+	category: string
+	detail: string
+	price: number
+	date: string
+}
+
+export const AnalyzeList = ({
+	consumptionData,
+}: {
+	consumptionData: analyzeDataType[]
+}) => {
+	return (
+		<div className='flex flex-col w-full h-full tablet:mt-[6rem] items-center justify-center font-extrabold text-white text-3xl tracking-[0] leading-normal]'>
+			<div className='text-black mb-3'>List</div>
+			{consumptionData.map((data) => (
+				<>
+					<div className='flex-initial text-[#51b1ff]'>{data.category}</div>
+				</>
+			))}
+		</div>
+	)
 }
