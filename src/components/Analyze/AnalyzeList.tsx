88fa9bc7142a1819a6eaf568bb8995477e68d912
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-06 20:17:27 +0900

    feat: :art: 리스트 색깔별 출력하도록 변경
    현재는 색깔 4가지로 돌려 쓰고 있는데, 유형이 확정나면 유형별 색깔로 옮기는게 낫지 않을까 싶음?

--- src/components/Analyze/AnalyzeList.tsx
+++ src/components/Analyze/AnalyzeList.tsx
@@ -5,6 +5,8 @@ interface analyzeDataType {
 	date: string
 }
 
+const color = ['#5fb1ff', '#fffc58', '#8fff5f', '#df0000']
+
 export const AnalyzeList = ({
 	consumptionData,
 }: {
@@ -18,7 +20,7 @@ export const AnalyzeList = ({
 				</div>
 				<div className='overscroll-y-auto overflow-hidden w-full h-full'>
 					<ul className='flex flex-col flex-initial w-full h-full mt-5 gap-10 overflow-y-scroll'>
-						{consumptionData.map((data) => (
+						{consumptionData.map((data, index) => (
 							<>
 								<li className='flex flex-col flex-initial text-black'>
 									<div className='flex flex-1 justify-between'>
@@ -30,7 +32,10 @@ export const AnalyzeList = ({
 										</div>
 									</div>
 									<div className='flex-1 w-full h-[35px] bg-[#e8e8e8] rounded-[10px] shadow-analyze-chart'>
-										<div className='w-1/2 h-[35px] bg-[#5fb1ff] rounded-[10px] shadow-[4px_0px_16px_0px_rgba(0,0,0,0.20)]"' />
+										<div
+											className={`w-1/2 h-[35px] rounded-[10px] shadow-[4px_0px_16px_0px_rgba(0,0,0,0.20)]"`}
+											style={{ backgroundColor: color[index % 4] }}
+										/>
 									</div>
 								</li>
 							</>
