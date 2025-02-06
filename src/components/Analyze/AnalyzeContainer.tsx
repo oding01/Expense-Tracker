5a2c3f8a6e83e51f8455fc35f0683ff46016653a
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-06 19:01:03 +0900

    feat: :art: 지출 금액, 시작일 - 종료일도 디자인 추가

--- src/components/Analyze/AnalyzeContainer.tsx
+++ src/components/Analyze/AnalyzeContainer.tsx
@@ -5,7 +5,10 @@ import { consumptionData } from '@/Mock/Mock'
 
 export const AnalyzeContainer = () => {
 	return (
-		<div className='relative flex flex-col gap-6 m-10 min-w-auto tablet:min-w-[320px] tablet:flex-col bg-white rounded-[3rem] shadow-white-box pb-[68px]'>
+		<div className='relative flex flex-col gap-6 m-10 min-w-auto tablet:min-w-[320px] tablet:flex-col bg-white rounded-[3rem] shadow-white-box py-[68px]'>
+			<div className='flex-initial text-[36px] pl-10 font-semibold text-black text-4xl tracking-[0] leading-[normal] whitespace-nowrap'>
+				지출 분석
+			</div>
 			<AnalyzeChoice />
 			<div className='flex flex-1 flex-col tablet:flex-row w-full justify-center items-center px-10 gap-10'>
 				<AnalyzeCharts consumptionData={consumptionData} />
--- src/components/Analyze/AnayzeChoice.tsx
+++ src/components/Analyze/AnayzeChoice.tsx
@@ -1,27 +1,51 @@
 export const AnalyzeChoice = () => {
 	return (
-		<div className='flex flex-initial tablet:flex-row gap-4 justify-start w-full'>
-			<div className='flex-1 flex flex-col justify-center items-center min-w-[15rem] w-full'>
-				<label className='flex-1' htmlFor='날짜'>
-					날짜
-				</label>
-				<div className='flex flex-col tablet:flex-row flex-1 border-2 p-1 rounded-3xl'>
-					{/* datepicker 사용 예정 */}
-					<input type='date' />
-					<input type='date' />
+		<div className='flex flex-initial flex-col tablet:flex-row gap-1 justify-start w-full tablet:gap-10 px-10'>
+			<div className='relative flex-1 flex flex-col justify-start items-center bg-[#f6f6f7] rounded-[18px] shadow-analyze-box w-full h-full p-[20px]'>
+				<div className='flex flex-col justify-between items-center tablet:flex-row flex-1 gap-2 w-full'>
+					<div className='flex flex-1 flex-col'>
+						<label
+							className='text-[#838383] text-base font-medium tracking-wide'
+							htmlFor='시작일'
+						>
+							시작일
+						</label>
+						<input
+							type='date'
+							className='text-2xl font-medium text-[#554a4a]'
+							value='2025-01-01'
+						/>
+					</div>
+					<div className='flex flex-col flex-1'>
+						<label
+							className='text-[#838383] text-base font-medium tracking-wide'
+							htmlFor='종료일'
+						>
+							종료일
+						</label>
+						<input
+							className='text-2xl font-medium text-[#554a4a]'
+							type='date'
+							value='2025-01-01'
+						/>
+					</div>
 				</div>
 			</div>
-			<div className='flex flex-col flex-1 items-center text-center min-w-[15rem]'>
-				<label className='flex-1' htmlFor='날짜'>
-					분류
-				</label>
-				<div className='flex gap-2 justify-center border-2 p-1 rounded-3xl'>
-					<button className='flex-initial rounded-2xl p-0.5 text-sm'>
-						카테고리
-					</button>
-					<button className='flex-initial rounded-2xl p-0.5 text-sm'>
-						장소
-					</button>
+			<div className='relative flex flex-1 flex-row justify-start items-center bg-[#f6f6f7] rounded-[18px] shadow-analyze-box w-full h-full p-[20px]'>
+				<div className='ml-3 flex flex-1 flex-col'>
+					<label
+						className='text-[#838383] text-base font-medium tracking-wide'
+						htmlFor='총 지출액'
+					>
+						총 지출액
+					</label>
+					<img
+						src='./src/images/totalPrice.svg'
+						className='w-[34px] h-[32px]'
+					></img>
+				</div>
+				<div className='flex justify-between mr-5'>
+					<div className='font-semibold text-2xl'>1,000,000원</div>
 				</div>
 			</div>
 		</div>
