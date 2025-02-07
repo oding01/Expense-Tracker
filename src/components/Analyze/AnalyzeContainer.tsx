3f0a14d8b30911fd9c9349ceacb2ab555d5a5202
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-07 10:44:05 +0900

    AnalyzeList의 ProgressBar 퍼센트별 출력

--- src/components/Analyze/AnalyzeContainer.tsx
+++ src/components/Analyze/AnalyzeContainer.tsx
@@ -1,11 +1,11 @@
-import { AnalyzeProvider } from '@/components/Analyze/AnalyzeContext'
 import { AnalyzeCharts } from '@/components/Analyze/AnalyzeCharts'
 import { AnalyzeList } from '@/components/Analyze/AnalyzeList'
 import { AnalyzeChoice } from '@/components/Analyze/AnalyzeChoice'
+import { AnalyzeProvider } from '@/components/Analyze/AnalyzeContext'
 
 export const AnalyzeContainer = () => {
 	return (
-		<div className='relative flex flex-col gap-6 m-10 min-w-auto tablet:min-w-[320px] tablet:flex-col bg-white rounded-[3rem] shadow-white-box py-[68px]'>
+		<div className='relative flex flex-col gap-6 m-10 min-w-auto tablet:min-w-[320px] tablet:flex-col bg-gray-50 rounded-[3rem] shadow-2xl py-[50px]'>
 			<div className='flex-initial text-[36px] pl-10 font-semibold text-black text-4xl tracking-[0] leading-[normal] whitespace-nowrap'>
 				지출 분석
 			</div>
