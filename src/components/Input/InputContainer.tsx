30b548b52c9a3bc304d070f5d054b858e444efe6
이어진 <slimin92@naver.com>
2025-02-07 12:21:25 +0900

    feat: :sparkles: Input 컴포넌트 생성

---
+++ src/components/Input/InputContainer.tsx
@@ -0,0 +1,11 @@
+import Input from '@/components/Input/input'
+
+const InputContainer = () => {
+	return (
+		<div className='relative flex flex-col gap-6 m-10 min-w-auto tablet:min-w-[320px] tablet:flex-col bg-white rounded-[3rem] shadow-white-box'>
+			<Input />
+		</div>
+	)
+}
+
+export default InputContainer
---
+++ src/components/Input/input.tsx
@@ -0,0 +1,11 @@
+const Input = () => {
+	return (
+		<>
+			<h3>수입 및 지출 입력하기</h3>
+
+			<div className='relative rounded-[70px] w-64 bg-[#F7F7F8]'></div>
+		</>
+	)
+}
+
+export default Input
