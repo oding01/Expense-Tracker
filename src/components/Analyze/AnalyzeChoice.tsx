8e3f320185bd4e8fa13635f30f9f5ae21b114769
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-10 15:54:15 +0900

    분석 페이지 CSS 변경 index.css - breakpoint를 더 넣음(하드코딩 느낌 나는데.. 평가좀 해주십쇼) AnalyzeChoice - 최소한의 찌그러지는거 방지 AnalyzeList - Progress Bar Transition 추가

--- src/components/Analyze/AnalyzeChoice.tsx
+++ src/components/Analyze/AnalyzeChoice.tsx
@@ -7,39 +7,39 @@ export const AnalyzeChoice = () => {
 	return (
 		<div className='flex flex-initial flex-col tablet:flex-row gap-1 justify-start w-full tablet:gap-10 px-10'>
 			<div className='relative flex-1 flex flex-col justify-start items-center bg-[#f6f6f7] rounded-[10px] shadow-analyze-box w-full h-full p-[20px]'>
-				<div className='flex flex-col justify-between items-center tablet:flex-row flex-1 gap-2 w-full'>
+				<div className='flex flex-col justify-between items-center analyzechoice:flex-row flex-1 gap-2 w-full'>
 					<div className='flex flex-1 flex-col'>
 						<label
-							className='text-[#838383] text-base font-medium tracking-wide'
+							className='text-[#838383] text-sm font-medium tracking-wide mobile:text-base'
 							htmlFor='시작일'
 						>
 							시작일
 						</label>
 						<input
 							type='date'
-							className='text-2xl font-medium text-[#554a4a]'
+							className='text-lg mobile:text-2xl font-medium text-[#554a4a]'
 							value='2025-01-01'
 						/>
 					</div>
 					<div className='flex flex-col flex-1'>
 						<label
-							className='text-[#838383] text-base font-medium tracking-wide'
+							className='text-[#838383] text-sm font-medium tracking-wide mobile:text-base'
 							htmlFor='종료일'
 						>
 							종료일
 						</label>
 						<input
-							className='text-2xl font-medium text-[#554a4a]'
+							className='text-lg mobile:text-2xl font-medium text-[#554a4a] '
 							type='date'
 							value='2025-01-01'
 						/>
 					</div>
 				</div>
 			</div>
-			<div className='relative flex flex-1 flex-row justify-start items-center bg-[#f6f6f7] rounded-[10px] shadow-analyze-box w-full h-full p-[20px]'>
-				<div className='ml-3 flex flex-1 flex-col'>
+			<div className='relative flex flex-1 flex-row justify-center items-center bg-[#f6f6f7] rounded-[10px] shadow-analyze-box w-full h-[160px] analyzechoice:h-full p-[20px]'>
+				<div className='ml-3 flex flex-1 flex-col h-full justify-center'>
 					<label
-						className='text-[#838383] text-base font-medium tracking-wide'
+						className='text-[#838383] text-sm mobile:text-base font-medium tracking-wide'
 						htmlFor='총 지출액'
 					>
 						총 지출액
@@ -49,8 +49,8 @@ export const AnalyzeChoice = () => {
 						className='w-[34px] h-[32px]'
 					></img>
 				</div>
-				<div className='flex justify-between mr-5'>
-					<div className='font-semibold text-2xl'>
+				<div className='flex justify-between mr-5 h-full items-center'>
+					<div className='font-semibold text-lg mobile:text-2xl'>
 						{addComma(totalPrice ?? 0)}원
 					</div>
 				</div>
