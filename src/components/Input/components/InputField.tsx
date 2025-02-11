a736c214cd11e480f3a06fa4fe6f758b1b8567e5
이어진 <slimin92@naver.com>
2025-02-11 12:08:26 +0900

    feat: :sparkles: 입력 페이지 - 확인 버튼 추가
    - 확인 버튼 추가 (hover, focus)
    - 카테고리 위치 변경에 따른 레이아웃 조정
    - 버튼 shadow css 추가

--- src/components/Input/components/InputField.tsx
+++ src/components/Input/components/InputField.tsx
@@ -21,14 +21,16 @@ const InputField = ({
 	const { register } = useFormContext<IInputFormData>()
 
 	return (
-		<div className={`mb-11 ${tagName === 'textarea' && 'flex-1'}`}>
+		<div
+			className={`flex flex-col mb-11 ${tagName === 'textarea' && 'flex-1'}`}
+		>
 			{/* 라벨 영역 */}
 			<div className='flex justify-between items-center mb-2'>
 				<label className='text-2xl font-medium ml-0.5'>{label}</label>
 				{unit && <label>({unit})</label>}
 			</div>
 			<div
-				className={`relative flex rounded-[15px] w-full bg-[#F7F7F8] shadow-analyze-box tablet:flex-row flex-1 ${tagName === 'textarea' ? 'h-full' : 'h-16'}`}
+				className={`relative flex rounded-[15px] w-full bg-[#F7F7F8] shadow-analyze-box tablet:flex-row flex-1`}
 			>
 				{/* tagName에 따른 input, textarea 출력 */}
 				{tagName === 'textarea' ? (
@@ -39,7 +41,7 @@ const InputField = ({
 				) : (
 					<input
 						type={type}
-						className='text-xl px-5 w-full rounded-[15px] focus:ring-2 focus:ring-inset focus:ring-[#5FB1FF] focus:outline-none bg-transparent'
+						className='text-xl px-5 w-full h-16 rounded-[15px] focus:ring-2 focus:ring-inset focus:ring-[#5FB1FF] focus:outline-none bg-transparent'
 						{...register(name)}
 					/>
 				)}
--- src/components/Input/components/InputForm.tsx
+++ src/components/Input/components/InputForm.tsx
@@ -11,11 +11,20 @@ const InputForm = ({ children }: { children: React.ReactNode }) => {
 	}
 
 	return (
-		<div className='pt-[50px] pb-[80px] px-10'>
+		<div className='pt-[40px] pb-[60px] px-10'>
 			<h2 className='text-black text-4xl font-semibold mb-10'>
 				수입 및 지출 입력하기
 			</h2>
-			<form onSubmit={handleSubmit(onSubmit)}>{children}</form>
+			<form onSubmit={handleSubmit(onSubmit)} className='h-full'>
+				{children}
+				<button
+					type='submit'
+					className='flex items-center justify-center w-36 h-16 rounded-2xl text-2xl tracking-wide shadow-submit-button bg-[#5FB1FF] text-white float-right
+				cursor-pointer hover:bg-blue-400 active:shadow-inner active:translate-y-0.5 active:bg-blue-400 transition-all duration-150'
+				>
+					확인
+				</button>
+			</form>
 		</div>
 	)
 }
