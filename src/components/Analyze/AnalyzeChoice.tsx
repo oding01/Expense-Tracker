d0d0bbbfa63266f3450968e020518d18041db8c9
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-02-12 18:55:43 +0900

    Feat - DatePicker 필요한 기능만 쓰는 DatePick 컴포넌트 생성
    DatePick - 라이브러리에서 필요한거만 따로 빼놓음
    * property
       * selectedDate : 필수, Date 형식의 기본값을 넣어줘야함
       * minDate: 선택, 범위 있을 때만 필요함, 기본 값 1900년 1월 1일
       * MaxDate: 선택, 범위 있을 때만 필요함, 기본 값 오늘
       * handleSelect : 필수, 선택했을 때, 날짜를 저장하는 함수 들어감
       * textSize : 선택, 텍스트 사이즈 조절할때 쓰세요, 기본값 'text-md'
       * iconSize : 선택, icon width, heigh 조절, 기본값 20px
    * AnalyzeChoice - DatePick 쓰는 대표 사례 만들어둠, 참고하면 될덧
    * index.css - DatePicker의 input, calendar 세부 조정, analyzechoice breakpoint 삭제

--- src/components/Analyze/AnalyzeChoice.tsx
+++ src/components/Analyze/AnalyzeChoice.tsx
@@ -1,43 +1,55 @@
 import { AnalyzeContext } from '@/components/Analyze/AnalyzeContext'
+import { DatePick } from '@/utils/DatePick'
 import { addComma } from '@/utils/money'
 import { useContext } from 'react'
 
 export const AnalyzeChoice = () => {
-	const { totalPrice } = useContext(AnalyzeContext)
+	const {
+		totalPrice,
+		startDate,
+		endDate,
+		handleSetStartDate,
+		handleSetEndDate,
+	} = useContext(AnalyzeContext)
+
 	return (
 		<div className='flex flex-initial flex-col tablet:flex-row gap-1 justify-start w-full tablet:gap-10 px-10'>
 			<div className='relative flex-1 flex flex-col justify-start items-center bg-[#f6f6f7] rounded-[10px] shadow-analyze-box w-full h-full p-[20px]'>
-				<div className='flex flex-col justify-between items-center analyzechoice:flex-row flex-1 gap-2 w-full'>
-					<div className='flex flex-1 flex-col'>
+				<div className='flex flex-row justify-between items-center flex-1 gap-2 w-full flex-wrap'>
+					<div className='flex flex-3 flex-col ml-1 tablet:flex-1'>
 						<label
 							className='text-[#838383] text-sm font-medium tracking-wide mobile:text-base'
 							htmlFor='시작일'
 						>
 							시작일
 						</label>
-						<input
-							type='date'
-							className='text-lg mobile:text-2xl font-medium text-[#554a4a]'
-							value='2025-01-01'
+						<DatePick
+							selectedDate={startDate ?? new Date()}
+							handleSelect={handleSetStartDate}
+							textSize='text-lg'
+							maxDate={endDate}
 						/>
 					</div>
-					<div className='flex flex-col flex-1'>
-						<label
-							className='text-[#838383] text-sm font-medium tracking-wide mobile:text-base'
-							htmlFor='종료일'
-						>
-							종료일
-						</label>
-						<input
-							className='text-lg mobile:text-2xl font-medium text-[#554a4a] '
-							type='date'
-							value='2025-01-01'
-						/>
+					<div className='flex flex-col flex-2 justify-center tablet:flex-1 ml-1'>
+						<div className='flex flex-col tablet:mr-1'>
+							<label
+								className='text-[#838383] text-sm font-medium tracking-wide mobile:text-base'
+								htmlFor='종료일'
+							>
+								종료일
+							</label>
+							<DatePick
+								selectedDate={endDate ?? new Date()}
+								handleSelect={handleSetEndDate}
+								textSize='text-lg'
+								minDate={startDate}
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div className='relative flex flex-1 flex-row justify-center items-center bg-[#f6f6f7] rounded-[10px] shadow-analyze-box w-full h-[160px] analyzechoice:h-full p-[20px]'>
-				<div className='ml-3 flex flex-1 flex-col h-full justify-center'>
+			<div className='relative flex flex-1 flex-row justify-center items-center bg-[#f6f6f7] rounded-[10px] shadow-analyze-box w-full p-[20px]'>
+				<div className='ml-1 flex flex-3 flex-col h-full justify-center tablet:flex-1'>
 					<label
 						className='text-[#838383] text-sm mobile:text-base font-medium tracking-wide'
 						htmlFor='총 지출액'
@@ -49,8 +61,8 @@ export const AnalyzeChoice = () => {
 						className='w-[34px] h-[32px]'
 					></img>
 				</div>
-				<div className='flex justify-between mr-5 h-full items-center'>
-					<div className='font-semibold text-lg mobile:text-2xl'>
+				<div className='flex h-full items-center flex-2 justify-start tablet:flex-initial tablet:mr-1'>
+					<div className='font-semibold text-lg mobile:text-2xl whitespace-nowrap'>
 						{addComma(totalPrice ?? 0)}원
 					</div>
 				</div>
--- src/components/Analyze/AnalyzeContext.tsx
+++ src/components/Analyze/AnalyzeContext.tsx
@@ -9,6 +9,10 @@ interface AnalyzeContextType {
 	progressBarWidth?: string[]
 	consumptionCombinedData?: analyzeCombinedDataType[]
 	doughnutChartData?: ChartDataType[]
+	startDate?: Date
+	endDate?: Date
+	handleSetStartDate: (date: Date | null) => void
+	handleSetEndDate: (date: Date | null) => void
 }
 
 interface ChartDataType {
@@ -18,7 +22,10 @@ interface ChartDataType {
 	color: string
 }
 
-export const AnalyzeContext = createContext<AnalyzeContextType>({})
+export const AnalyzeContext = createContext<AnalyzeContextType>({
+	handleSetStartDate: () => {},
+	handleSetEndDate: () => {},
+})
 
 const calculateWidth = (
 	consumptionData: analyzeDataType[],
@@ -37,6 +44,8 @@ export const AnalyzeProvider = ({
 }) => {
 	// Store에서 Data 꺼내와서 변수로 저장
 	const consumptionData = useAnalyzeStore((state) => state.consumptionData)
+	const startDate = useAnalyzeStore((state) => state.startDate)
+	const endDate = useAnalyzeStore((state) => state.endDate)
 
 	//Store에서 consumptionCombinedData 꺼내와서 변수로 저장
 	const consumptionCombinedData = useAnalyzeStore(
@@ -57,6 +66,24 @@ export const AnalyzeProvider = ({
 		0,
 	)
 
+	const { setStartDate, setEndDate } = useAnalyzeStore((state) => state.actions)
+
+	const handleSetStartDate = (date: Date | null) => {
+		if (!date) {
+			alert('날짜를 선택해주세요')
+			return
+		}
+		setStartDate(date)
+	}
+
+	const handleSetEndDate = (date: Date | null) => {
+		if (!date) {
+			alert('날짜를 선택해주세요')
+			return
+		}
+		setEndDate(date)
+	}
+
 	const progressBarWidth = calculateWidth(consumptionData, totalPrice)
 	return (
 		<AnalyzeContext.Provider
@@ -66,6 +93,10 @@ export const AnalyzeProvider = ({
 				progressBarWidth,
 				consumptionCombinedData,
 				doughnutChartData,
+				startDate,
+				endDate,
+				handleSetStartDate,
+				handleSetEndDate,
 			}}
 		>
 			{children}
