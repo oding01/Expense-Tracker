49ea11fc20fe949d8ebd3c3eb54d8bfdd56f68fb
이어진 <slimin92@naver.com>
2025-02-12 10:06:33 +0900

    feat: :sparkles: Supabase 추가 및 Submit 기능 구현
    🔥[변경 사항]
    - Supabase 설치
    - database.types.ts : 데이터베이스 테이블 타입 정의
    - api.ts : 수입 및 지출 데이터 insert api 로직
    - InputForm에 Mutation으로 데이터 insert
    - Layout에 QueryClientProvider 추가
    - supabse.ts : Supabase 접근 로직

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -1,13 +1,18 @@
 import Header from '@/shared/Header'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Outlet } from 'react-router'
+
+const queryClient = new QueryClient()
 
 const Layout = () => {
 	return (
-    <div className='flex flex-col min-h-screen min-w-full items-center relative'>
+		<div className='flex flex-col min-h-screen min-w-full items-center relative'>
 			<Header />
-      {/* flex-1로 Header를 제외한 남은 영역 채우기 */}
+			{/* flex-1로 Header를 제외한 남은 영역 채우기 */}
 			<div className='w-full pt-16 flex-1'>
-				<Outlet />
+				<QueryClientProvider client={queryClient}>
+					<Outlet />
+				</QueryClientProvider>
 			</div>
 		</div>
 	)
