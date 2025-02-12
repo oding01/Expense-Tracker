ebbcd1d4e756ea27993a7f826dceee07d75ed369
이어진 <slimin92@naver.com>
2025-02-12 14:32:43 +0900

    fix: :bug: 배포로 인한 Dead Code 수정

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,40 +1,38 @@
-import { ICategory, IInputFormData } from "@/types/type"
-import { Json } from "database.types"
-import supabase from "supabase/supabase"
+import { ICategory, IInputFormData } from '@/types/type'
+import { Json } from 'database.types'
+import supabase from 'supabase/supabase'
 
 interface RequestValue {
-  input_type: string
-  category: Json
-  amount: number
-  place: string
-  memo: string
-  date: string
+	input_type: string
+	category: Json
+	amount: number
+	place: string
+	memo: string
+	date: string
 }
 
-const categoryToJson = (category : ICategory): Json => ({
-  id: category.id,
-  name: category.name,
-  value: category.value,
+const categoryToJson = (category: ICategory): Json => ({
+	id: category.id,
+	name: category.name,
+	value: category.value,
 })
 
 export const insertData = async (input: IInputFormData) => {
-  const requestData : RequestValue = {
-    input_type: input.inputType,
-    category: categoryToJson(input.category),
-    amount: input.amount,
-    place: input.place,
-    memo: input.memo,
-    date: input.date,
-  }
+	const requestData: RequestValue = {
+		input_type: input.inputType,
+		category: categoryToJson(input.category),
+		amount: input.amount,
+		place: input.place,
+		memo: input.memo,
+		date: input.date,
+	}
 
-  const { data, error } = await supabase
-    .from('expenseList')
-    .insert(requestData)
+	const { data, error } = await supabase.from('expenseList').insert(requestData)
 
-  if (error) {
-    throw error
-  } else {
-    console.log('Data inserted Success')
-  }
-  return true
+	if (error) {
+		throw error
+	} else {
+		console.log('Data inserted Success: ', data)
+	}
+	return true
 }
